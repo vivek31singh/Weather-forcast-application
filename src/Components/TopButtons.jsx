c179985e7bf1f36040fa32e018b1bc4637957075
vivek singh <vickydigari6154@gmail.com>
2023-04-22 14:33:00 +0530

    feat: completed with the project!

--- src/Components/TopButtons.jsx
+++ src/Components/TopButtons.jsx
@@ -4,7 +4,7 @@ function TopButtons({ setQuery }) {
   const cities = [
     {
       id: 1,
-      title: "london",
+      title: "Sydney",
     },
     {
       id: 2,
@@ -12,7 +12,7 @@ function TopButtons({ setQuery }) {
     },
     {
       id: 3,
-      title: "Tokyo",
+      title: "Chandigarh",
     },
     {
       id: 4,
@@ -20,7 +20,7 @@ function TopButtons({ setQuery }) {
     },
     {
       id: 5,
-      title: "Sydney",
+      title: "Uttrakhand",
     },
   ];
 
