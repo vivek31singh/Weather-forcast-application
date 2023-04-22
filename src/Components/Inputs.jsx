7d410b0c10c125e7dde3f62b9e0e1fa5274c5068
vivek singh <vickydigari6154@gmail.com>
2023-04-22 12:29:45 +0530

    feat: completed with the project!

--- src/Components/Inputs.jsx
+++ src/Components/Inputs.jsx
@@ -40,7 +40,7 @@ const handleUnitsChange=(e)=>{
           onChange={(e) => setCity(e.currentTarget.value)}
           type="text"
           placeholder="search for city..."
-          className="text-xl font-light p-2 focus:outline-none w-full shadow-xl capitalize placeholder:lowercase max-sm:w-60 ml-5"
+          className="text-xl font-light p-2 focus:outline-none w-full shadow-xl capitalize placeholder:lowercase max-sm:w-40"
         />
         <BsSearch
           size={25}
@@ -57,7 +57,7 @@ const handleUnitsChange=(e)=>{
       <div className="flex flex-row w-1/4 items-center justify-center max-sm:ml-10">
         <button
           name="metric"
-          className="text-xl text-white font-light hover:scale-125 transition ease-out max-sm:ml-4"
+          className="text-xl text-white font-light hover:scale-125 transition ease-out max-sm:ml-20"
           onClick={handleUnitsChange}
         >
           °C
